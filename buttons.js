d35076280fe5e1f17721576ffb4bd349cdac06dd
Bartek <75677928+yis-san@users.noreply.github.com>
2021-02-21 16:29:56 +0100

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,24 @@
+function rtask1() {
+    window.open("rtask1.html")
+}
+function rtask2() {
+    window.open("rtask2.html")
+}
+function rtask3() {
+    window.open("rtask3.html")
+}
+function rtask4() {
+    window.open("rtask4.html")
+}
+function rtask5() {
+    window.open("rtask5.html")
+}
+function rtask6() {
+    window.open("rtask6.html")
+}
+function rtask7() {
+    window.open("rtask7.html")
+}
+function rtask8() {
+    window.open("rtask8.html")
+}
